f3e1f9038d07346753c1c5e6e398479154141256
Nick Torres <nickrtorres@icloud.com>
2020-04-01 01:10:26 -0700

    nl: restructure nl into lib.rs and bin/nl.rs
    Signed-off-by: Nick Torres <nickrtorres@icloud.com>

---
+++ src/bin/nl.rs
@@ -0,0 +1,121 @@
+// This implementation references (1) apple's and (2) illumos's. As such, both
+// copyrights are provided below for brevity:
+//
+// (1)
+// Copyright (c) 1999 The NetBSD Foundation, Inc.
+// All rights reserved.
+//
+// This code is derived from software contributed to The NetBSD Foundation
+// by Klaus Klein.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. All advertising materials mentioning features or use of this software
+//    must display the following acknowledgement:
+//        This product includes software developed by the NetBSD
+//        Foundation, Inc. and its contributors.
+// 4. Neither the name of The NetBSD Foundation nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+// ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+// BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// (2)
+// CDDL HEADER START
+//
+// The contents of this file are subject to the terms of the
+// Common Development and Distribution License, Version 1.0 only
+// (the "License").  You may not use this file except in compliance
+// with the License.
+//
+// You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+// or http://www.opensolaris.org/os/licensing.
+// See the License for the specific language governing permissions
+// and limitations under the License.
+//
+// When distributing Covered Code, include this CDDL HEADER in each
+// file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+// If applicable, add the following below this CDDL HEADER, with the
+// fields enclosed by brackets "[]" replaced with your own identifying
+// information: Portions Copyright [yyyy] [name of copyright owner]
+//
+// CDDL HEADER END
+//
+//
+// Copyright 1995 Sun Microsystems, Inc.  All rights reserved.
+// Use is subject to license terms.
+//
+//
+//	Copyright (c) 1984, 1986, 1987, 1988, 1989 AT&T
+//	  All Rights Reserved
+use clap::{App, Arg};
+use lazy_static::lazy_static;
+use program::Program;
+use std::fs::File;
+use std::io::{self, stderr, stdin, BufReader, Write};
+use std::process::exit;
+
+extern crate nl_rs;
+use nl_rs::Cli;
+
+lazy_static! {
+    static ref NL: Program = Program::new("nl");
+}
+
+fn try_main<'a>(cli: Cli, filename: Option<&str>) -> Result<(), io::Error> {
+    let stdin = stdin();
+    match filename {
+        Some(f) => {
+            let file = File::open(f)?;
+            cli.filter(&mut BufReader::new(file))
+        }
+        None => cli.filter(&mut stdin.lock()),
+    }
+}
+
+fn main() {
+    let args = App::new(NL.name)
+        .version("0.0.1")
+        .arg(Arg::with_name("blanks").short("l").takes_value(true))
+        .arg(Arg::with_name("body-type").short("b").takes_value(true))
+        .arg(Arg::with_name("delim").short("d").takes_value(true))
+        .arg(Arg::with_name("file").index(1).takes_value(true))
+        .arg(Arg::with_name("footer-type").short("f").takes_value(true))
+        .arg(Arg::with_name("format").short("n").takes_value(true))
+        .arg(Arg::with_name("header-type").short("h").takes_value(true))
+        .arg(Arg::with_name("increment").short("i").takes_value(true))
+        .arg(Arg::with_name("initial-value").short("v").takes_value(true))
+        .arg(Arg::with_name("restart-at-page").short("p"))
+        .arg(Arg::with_name("width").short("w").takes_value(true))
+        .get_matches_safe();
+
+    let matches = match args {
+        Ok(m) => m,
+        Err(e) => {
+            let _ = write!(stderr(), "{}", e);
+            exit(0);
+        }
+    };
+
+    let cli = Cli::new(&matches).unwrap_or_else(|e| NL.perror(e));
+
+    if let Err(e) = try_main(cli, matches.value_of("file")) {
+        NL.perror(e);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,94 +1,24 @@
-// This implementation references (1) apple's and (2) illumos's. As such, both
-// copyrights are provided below for brevity:
-//
-// (1)
-// Copyright (c) 1999 The NetBSD Foundation, Inc.
-// All rights reserved.
-//
-// This code is derived from software contributed to The NetBSD Foundation
-// by Klaus Klein.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions
-// are met:
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-// 3. All advertising materials mentioning features or use of this software
-//    must display the following acknowledgement:
-//        This product includes software developed by the NetBSD
-//        Foundation, Inc. and its contributors.
-// 4. Neither the name of The NetBSD Foundation nor the names of its
-//    contributors may be used to endorse or promote products derived
-//    from this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
-// ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
-// BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
-//
-// (2)
-// CDDL HEADER START
-//
-// The contents of this file are subject to the terms of the
-// Common Development and Distribution License, Version 1.0 only
-// (the "License").  You may not use this file except in compliance
-// with the License.
-//
-// You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
-// or http://www.opensolaris.org/os/licensing.
-// See the License for the specific language governing permissions
-// and limitations under the License.
-//
-// When distributing Covered Code, include this CDDL HEADER in each
-// file and include the License file at usr/src/OPENSOLARIS.LICENSE.
-// If applicable, add the following below this CDDL HEADER, with the
-// fields enclosed by brackets "[]" replaced with your own identifying
-// information: Portions Copyright [yyyy] [name of copyright owner]
-//
-// CDDL HEADER END
-//
-//
-// Copyright 1995 Sun Microsystems, Inc.  All rights reserved.
-// Use is subject to license terms.
-//
-//
-//	Copyright (c) 1984, 1986, 1987, 1988, 1989 AT&T
-//	  All Rights Reserved
-use clap::{App, Arg, ArgMatches};
-use lazy_static::lazy_static;
-use program::Program;
+use clap::ArgMatches;
 use regex::{self, Regex};
 use std::error;
 use std::fmt;
-use std::fs::File;
-use std::io::{self, stderr, stdin, stdout, BufRead, BufReader, Write};
+use std::io::{self, stdout, BufRead, Write};
 use std::num;
-use std::process::exit;
-
-#[derive(Debug)]
-enum NumberingType {
-    All,
-    NonEmpty,
-    None,
-    Regex(Regex),
-}
 
 #[derive(PartialEq, Debug)]
-enum NlError<'a> {
+/// Variants for what can go wrong when running `nl(1)`
+pub enum NlError<'a> {
+    /// `Regex::new(Regex)` failed. This is likely do to an invalid Regex
     BadRegex(regex::Error),
+    /// The Regex option was specified, but no regex was provided
     EmptyRegex,
+    /// A non-canonical (left justified w/o zeros, right justified w/o zeros, right justified w/
+    /// zeros) numbering format  was given
     IllegalFormat(&'a str),
+    /// A non-canonical (all, non-empty, none, regex) numbering type was given
     IllegalNumberingType(&'a str),
+    /// An option that required an integer was specified, but a non-integral type was given as the
+    /// parameter
     InvalidNumber,
 }
 
@@ -113,9 +43,21 @@ impl<'a> fmt::Display for NlError<'a> {
 }
 
 impl<'a> From<num::ParseIntError> for NlError<'a> {
-    fn from(err: num::ParseIntError) -> NlError<'a> {
+    fn from(_err: num::ParseIntError) -> NlError<'a> {
         NlError::InvalidNumber
     }
+}
+
+#[derive(Debug)]
+enum NumberingType {
+    /// Number everyline for the given file
+    All,
+    /// Number everyline that contains > 1 '\n' character
+    NonEmpty,
+    /// Don't number any lines
+    None,
+    /// Number lines that `Regex::match(Regex)` holds
+    Regex(Regex),
 }
 
 impl NumberingType {
@@ -171,11 +113,8 @@ impl LineNumberFormat {
     }
 }
 
-lazy_static! {
-    static ref NL: Program = Program::new("nl");
-}
-
-struct Cli<'a> {
+/// An opaque structure to store command line options specified by the user.
+pub struct Cli<'a> {
     blanks: u32,
     body: NumberingType,
     delim: &'a str,
@@ -189,7 +128,12 @@ struct Cli<'a> {
 }
 
 impl<'a> Cli<'a> {
-    fn new(args: &'a ArgMatches) -> Result<Self, NlError<'a>> {
+    /// Parsing command line options is tricky. The options are *optional*. Additionally,
+    /// a user can provide invalid input (e.g. empty input, non numeric input for a numeric option,
+    /// etc.). In anycase, we need to deterministically evaluate what the user provides and store it
+    /// when it's valid, propogate an error when it's invalid, or default to a well-defined value when
+    /// it's absent.
+    pub fn new(args: &'a ArgMatches) -> Result<Self, NlError<'a>> {
         let body = match args.value_of("body") {
             Some(b) => NumberingType::from_opt(b)?,
             None => NumberingType::NonEmpty,
@@ -210,7 +154,6 @@ impl<'a> Cli<'a> {
             None => LineNumberFormat::Rn,
         };
 
-        // TODO allow Box'd errors to avoid map_err
         let increment = match args.value_of("increment") {
             Some(i) => i.parse::<u32>()?,
             None => 1,
@@ -235,8 +178,9 @@ impl<'a> Cli<'a> {
         })
     }
 
-    fn filter<T: BufRead>(self, mut input: T) -> Result<(), io::Error> {
-        let mut buf = String::with_capacity(1024);
+    /// Output a file to `stdout` annotated with numbering in the style specified by
+    /// the user through command line flags.
+    pub fn filter<T: BufRead>(self, input: T) -> Result<(), io::Error> {
         let mut num = self.startnum;
         for line in input.lines() {
             let line = line?;
@@ -262,46 +206,6 @@ impl<'a> Cli<'a> {
         }
 
         Ok(())
-    }
-}
-
-fn main() {
-    let args = App::new(NL.name)
-        .version("0.0.1")
-        .arg(Arg::with_name("blanks").short("l").takes_value(true))
-        .arg(Arg::with_name("body-type").short("b").takes_value(true))
-        .arg(Arg::with_name("delim").short("d").takes_value(true))
-        .arg(Arg::with_name("file").index(1).takes_value(true))
-        .arg(Arg::with_name("footer-type").short("f").takes_value(true))
-        .arg(Arg::with_name("format").short("n").takes_value(true))
-        .arg(Arg::with_name("header-type").short("h").takes_value(true))
-        .arg(Arg::with_name("increment").short("i").takes_value(true))
-        .arg(Arg::with_name("initial-value").short("v").takes_value(true))
-        .arg(Arg::with_name("restart-at-page").short("p"))
-        .arg(Arg::with_name("width").short("w").takes_value(true))
-        .get_matches_safe();
-
-    let matches = match args {
-        Ok(m) => m,
-        Err(e) => {
-            let _ = write!(stderr(), "{}", e);
-            exit(0);
-        }
-    };
-
-    let cli = Cli::new(&matches).unwrap_or_else(|e| NL.perror(e));
-
-    let stdin = stdin();
-    let result = match matches.value_of("file") {
-        Some(f) => match File::open(f) {
-            Ok(f) => cli.filter(&mut BufReader::new(f)),
-            Err(e) => NL.perror(format!("{}: {}", f, e)),
-        },
-        None => cli.filter(&mut stdin.lock()),
-    };
-
-    if let Err(e) = result {
-        NL.perror(e);
     }
 }
 
